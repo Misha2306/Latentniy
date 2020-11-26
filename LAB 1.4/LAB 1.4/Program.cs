b0ae7a8cca67ccd43be2aaaa6e047f18626519e1
Misha <72248956+Misha2306@users.noreply.github.com>
2020-11-26 15:15:12 +0200

    Add files via upload

---
+++ LAB 1.4/LAB 1.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Number
+{
+    class mathematics
+    {
+        static void Main()
+        {
+            Console.WriteLine("Вибери число: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Число вибране: " + number);
+            double round = Math.Round(number);
+            Console.WriteLine("Заокруглення до найбільшого значення: " + round);
+            double floor = Math.Floor(number);
+            Console.WriteLine("Заокруглення до меншого значення: " + floor);
+        }
+    }
+}
---
+++ LAB 1.6/LAB 1.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Number
+{
+    class Mathematics
+    {
+        static void Main()
+        {
+            Console.WriteLine("Вибери число (d>0): ");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Число вибране: " + d);
+            if (d < 0)
+            {
+                Console.WriteLine("Наш код не буде працювати. Будь ласка, виберіть інше значення. Пам'ятайте, що d не повинно бути менше за 0");
+            }
+            else
+            {
+                double s = Math.Ceiling(d);
+                Console.WriteLine("Найбільше значення d : " + s);
+                double square_root = Math.Sqrt(s);
+                Console.WriteLine("Корінь квадратний з S дорівнює: " + square_root);
+
+            }
+
+        }
+    }
+}
