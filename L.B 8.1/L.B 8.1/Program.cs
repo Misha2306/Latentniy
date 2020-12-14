08f2beed4c22082b57d7a6c69cf6f30f5f0aa4a3
Misha <72248956+Misha2306@users.noreply.github.com>
2020-12-14 16:06:46 +0200

    Add files via upload

---
+++ L.B 8.1/L.B 8.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo("/Users/rs/Desktop/MishaLNU:LNU_Epam");
+
+            int dir_num = 0; 
+            DirectoryInfo[] directories = dir.GetDirectories(); 
+            foreach (var v in directories)
+            {
+                dir_num++; 
+                Console.WriteLine($"{dir_num}. {v}");
+                DirectoryInfo dirhas = new DirectoryInfo(v.FullName); 
+                DirectoryInfo[] directoryHas = dirhas.GetDirectories(); 
+              
+                int files_in_num = 0;
+                foreach (var item in directoryHas) 
+                {
+                    files_in_num++;
+                    Console.WriteLine($"  {files_in_num}. {item.FullName}");
+                }
+            }
+            Console.WriteLine("\nAll files shown:");
+            FileInfo[] files = dir.GetFiles();
+            foreach (var value in files)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+
+    }
+
+}
---
+++ L.B 8.2/L.B 8.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+namespace Lab_8._2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the name of file you want to find: ");
+            string from_user = Console.ReadLine();
+            string DirectoryName = "/Users/rs/Desktop/University:Learning/Theory/Literature";
+            DirectoryInfo directory = new DirectoryInfo(DirectoryName);
+            string[] requestedfiles = Directory.GetFiles(DirectoryName, $"*{from_user}*.txt", SearchOption.AllDirectories);
+            
+            if (requestedfiles.Length == 0)
+            {
+                Console.WriteLine("Can not find file. Try one more time.");
+            }
+            else
+            {
+                foreach (string file in requestedfiles)
+                {
+                    Console.WriteLine("Yay, we found it!" + file);
+                }
+            }
+        }
+    }
+}
